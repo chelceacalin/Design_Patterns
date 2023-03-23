46954183519768e7ce4fe1273f3160d09691f8e7
Calin <chelceacalin1@gmail.com>
2023-03-23 17:43:37 +0200

    Simple Singleton

---
+++ src/Singleton/SimpleSingleton/main/main.java
@@ -0,0 +1,34 @@
+package Singleton.SimpleSingleton.main;
+
+import Singleton.SimpleSingleton.classes.BazaDeDate;
+import Singleton.SimpleSingleton.classes.Secretariat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public static void main(String[] args) {
+
+        List< Object> list=new ArrayList<>();
+        Secretariat s1=Secretariat.getInstanta(5,10);
+        Secretariat s2=Secretariat.getInstanta(6,5);
+        list.add(s1);
+        list.add(s2);
+        list.stream().forEach(elem-> System.out.println(elem.toString()));
+        s2.setNumarAngajati(69);
+        list.stream().forEach(elem-> System.out.println(elem.toString()));
+
+
+        BazaDeDate b1=BazaDeDate.getInstanta();
+        BazaDeDate b2=BazaDeDate.getInstanta();
+
+        list.add(b1);
+        list.add(b2);
+
+        list.stream().forEach(elem-> System.out.println(elem.toString()));
+
+        b2.setDimensiuneDate(50);
+        list.stream().forEach(elem-> System.out.println(elem.toString()));
+
+    }
+}
