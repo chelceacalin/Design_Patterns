8e412219e625e4281a664f977196864360b8ab28
Calin <chelceacalin1@gmail.com>
2023-03-30 13:46:05 +0300

    Prototype

---
+++ src/creationale/Prototype/classes/IContCloneable.java
@@ -0,0 +1,8 @@
+package creationale.Prototype.classes;
+
+import creationale.Builder.classes.ContBancar;
+
+public interface IContCloneable {
+
+    IContCloneable clone();
+}
---
+++ src/creationale/Prototype/main/main.java
@@ -0,0 +1,23 @@
+package creationale.Prototype.main;
+
+import creationale.Prototype.classes.ContBancar;
+import creationale.Prototype.classes.IContCloneable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class main {
+    public static void main(String[] args) {
+        Map<String,Integer> mp=new HashMap<>();
+        mp.put("Doc1",4);
+        mp.put("Doc2",1);
+        IContCloneable cont=new ContBancar("Calin",mp,5);
+        ContBancar c2= (ContBancar) cont.clone();
+        c2.setDetinator("raresn");
+
+        System.out.println(cont.toString());
+
+
+        System.out.println(c2);
+    }
+}
